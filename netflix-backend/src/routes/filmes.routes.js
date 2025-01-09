c4176f7c88547625d5a78e877a6b3fd1f9667617
Samuel Gonçalves <samuelesilva@hotmail.com>
2025-01-09 18:06:25 -0300

    primeiro commit

---
+++ netflix-backend/src/routes/filmes.routes.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = express.Router()
+const Filme = require('../models/filme')
+
+
+//RECUPERAR TODOS OS REGISTROS
+router.get('/', async (req, res) => {
+    try {
+        
+        const filmes = await Filme.find({});
+        res.json({error: false, filmes})
+    } catch (err) {
+        res.json({ error: true, message: err.message })
+    }
+})
+
+// PEGAR SOMENTE O REGISTRO COM O ID
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const filme = await Filme.findById(id)
+        res.json({err: false, filme})
+    } catch (err) {
+    res.json({ error: true, message: err.message })
+        
+    }
+   
+})
+
+// CRIAR UM REGISTRO
+router.post('/', async (req, res) => {
+    try {
+        const filme = req.body
+        const response = await new Filme(filme).save()
+        res.json({ error: false, filme: response })
+    } 
+    catch (err) {
+        res.json({ error: true, message: err.message })
+    }
+
+})
+
+// ATUALIZAR UM REGISTRO SOMENTE COM ID
+router.put('/:id', async (req, res) => {
+try {
+    const id = req.params.id
+    const novo_filme = req.body
+
+    const filme = await Filme.findByIdAndUpdate(id, novo_filme)
+    res.json({error: false, filme })
+} catch (err) {
+    res.json({ error: true, message: err.message})
+}
+    
+    
+})
+
+// DELETAR UM REGISTRO SOMENTE COM ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const deleted = await Filme.findOneAndDelete(id)
+        res.json({error: false})
+    } catch (err) {
+        res.json({ error: true, message: err.message})
+    }
+    
+    
+})
+
+module.exports = router;
