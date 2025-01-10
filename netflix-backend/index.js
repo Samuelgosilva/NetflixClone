fe616186350f5ea8f52cdc9ed586dd6d7f17773c
Samuel Gonçalves <samuelesilva@hotmail.com>
2025-01-10 16:37:28 -0300

    criação de rotas de login e recuperando filmes na tela home

--- netflix-backend/index.js
+++ netflix-backend/index.js
@@ -2,15 +2,21 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const database = require('./src/services/database')
+const cors = require('cors')
 
 const app = express()
 
-const routes = require('./src/routes/filmes.routes')
+const filmeRoutes = require('./src/routes/filmes.routes')
+const usuarioRoutes = require('./src/routes/usuarios.routes')
 
-
+//MIDDLEWARES
 app.use(bodyParser.json())
+app.use(cors())
 app.use(morgan('dev'))
-app.use('/', routes)
+
+// ROUTES
+app.use('/', filmeRoutes)
+app.use('/usuario', usuarioRoutes)
 
 
 
